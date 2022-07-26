fe4a1fb4fd242faa2fcc2f5b93a304a96ce61528
Michel John <michel.santos@dataside.com.br>
2022-07-25 23:29:45 -0300

    config navigation / eslint/prettier / nativenase

--- android/app/src/main/java/com/next_president/MainActivity.java
+++ android/app/src/main/java/com/next_president/MainActivity.java
@@ -3,6 +3,8 @@ package com.next_president;
 import com.facebook.react.ReactActivity;
 import com.facebook.react.ReactActivityDelegate;
 import com.facebook.react.ReactRootView;
+// React native screens
+import android.os.Bundle;
 
 public class MainActivity extends ReactActivity {
 
@@ -13,6 +15,12 @@ public class MainActivity extends ReactActivity {
   @Override
   protected String getMainComponentName() {
     return "next_president";
+  }
+
+  // React native screens
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(null);
   }
 
   /**
---
+++ src/screens/SignIn/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { NativeBaseProvider, Box } from 'native-base';
+
+export function SignIn() {
+  return (
+    <NativeBaseProvider>
+      <Box>SignIn</Box>
+    </NativeBaseProvider>
+  );
+}
