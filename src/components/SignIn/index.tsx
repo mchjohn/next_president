527f39f394549865c8bf305ce092cffe7a93079a
Michel John <michel.john@hotmail.com>
2022-08-11 22:11:53 -0300

    removed facebook login + added filter in comment list

--- src/components/SignIn/index.tsx
+++ src/components/SignIn/index.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
-import { Button, Center, Icon, Modal, Text } from 'native-base';
-import MaterialIcons from 'react-native-vector-icons/MaterialIcons';
+import { Button, Center, Icon, Modal } from 'native-base';
 import AntDesign from 'react-native-vector-icons/AntDesign';
 
 import { useAuth } from '@contexts/AuthContext';
@@ -8,7 +7,7 @@ import { useModal } from '@contexts/ModalContext';
 
 export function SignIn() {
   const { showModal, closeModal } = useModal();
-  const { isLoading, signInWithGoogle, signInWithFacebook } = useAuth();
+  const { isLoading, signInWithGoogle } = useAuth();
 
   return (
     <Center>
@@ -29,22 +28,6 @@ export function SignIn() {
               leftIcon={<Icon as={<AntDesign name="google" />} size="2xl" color="gray.100" />}
             >
               Entrar com Google
-            </Button>
-
-            <Text textAlign="center" color="gray.400" my="2">
-              ou
-            </Text>
-
-            <Button
-              colorScheme="blue"
-              bgColor="#1478ef"
-              isLoading={isLoading}
-              spinnerPlacement="start"
-              isLoadingText="Entrando..."
-              onPress={signInWithFacebook}
-              leftIcon={<Icon as={<MaterialIcons name="facebook" />} size="2xl" color="gray.100" />}
-            >
-              Entrar com Facebook
             </Button>
           </Modal.Body>
         </Modal.Content>
